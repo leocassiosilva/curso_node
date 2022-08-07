12775139f4541f7101bc62326d9b50b29fdc7a59
leocassiosilva <leocassiosilva1234@gmail.com>
2022-08-06 21:06:26 -0300

    explorando console

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,22 @@
+const x = 10
+const y = "Leocassio"
+const z = [1,2]
+
+console.log(x, y, z)
+
+//contagem de impressões
+console.count(`O valor de x é: ${x}, contagem:`)
+console.count(`O valor de x é: ${x}, contagem:`)
+console.count(`O valor de x é: ${x}, contagem:`)
+console.count(`O valor de x é: ${x}, contagem:`)
+console.count(`O valor de x é: ${x}, contagem:`)
+
+
+//variavel de string
+console.log("o nome é %s, ele é programador", y)
+
+//limpar o console
+
+setTimeout(() =>{
+    console.clear()
+}, 2000)
