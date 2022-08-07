110d7c7efc6cbff7429b5d4452e87fc064badebb
leocassiosilva <leocassiosilva1234@gmail.com>
2022-08-07 00:27:59 -0300

    abstracao input

---
+++ 2_FUNDAMENTOS/10_abstracao_input/index.js
@@ -0,0 +1,18 @@
+// const inquirer = require('inquirer')
+import inquirer from "inquirer"
+
+
+inquirer.prompt([
+    {
+        name:'p1',
+        message:'Qual a primeira nota?', 
+    },
+    {
+        name:'p2',
+        message:'Qual a segunda nota?', 
+    }
+]).then((answers) => {
+    console.log(answers)
+    const  media = (parseInt(answers.p1) + parseInt(answers.p2))
+    console.log(`A média é: ${media}`)
+}).catch(err => console.log(err))
