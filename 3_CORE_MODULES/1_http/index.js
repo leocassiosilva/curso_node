ee3ac1c06b20fa315ff48fe9d3bbd2841239df91
leocassiosilva <leocassiosilva1234@gmail.com>
2022-08-07 11:08:50 -0300

    http

---
+++ 3_CORE_MODULES/1_http/index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+
+const port = 3000   
+
+const server = http.createServer((req, res) => {
+    res.write('Oi HTTP')
+    res.end()
+})
+
+server.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
