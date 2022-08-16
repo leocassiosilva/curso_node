8f0066bfbfe232ee1d363f3545f5864421bbaaa9
leocassiosilva <leocassiosilva1234@gmail.com>
2022-08-15 23:22:52 -0300

    renomeando arquivo txt

---
+++ 3_CORE_MODULES/9_renomenando_arquivo/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+
+fs.rename('arquivo.txt', 'novoarquivo.txt', function(err){
+
+    if(err){
+        console.log(err)
+        return 
+    }
+
+    console.log("Arquivo renomeado!")
+});
